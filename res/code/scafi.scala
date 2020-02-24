53a3300cb6fb29203e947ed6bb8a8b9d0c2a960c
Niccolò Maltoni <nicco.mlt@gmail.com>
2020-02-24 20:45:28 +0100

    feat: aggregate programming chapter done

---
+++ res/code/scafi.scala
@@ -0,0 +1,8 @@
+// valutazione di un valore costante
+"Hello, world!"
+
+// lettura di un valore da un sensore
+sens[Double]("temp")
+
+// conteggio del numero di round per ogni device
+rep(0) { x => x+1 }
